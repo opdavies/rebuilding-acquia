59f56a674c947aa053d8d3369c5b05aca19e6de0
Oliver Davies <oliver@oliverdavies.uk>
2018-12-22 01:18:06 +0000

    Fix build error

--- src/components/EnvironmentCards.vue
+++ src/components/EnvironmentCards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-6">
     <div class="flex flex-wrap -mx-4 -mb-6">
-      <div v-for="environment in environments" class="w-full md:w-1/2 xl:w-1/3 px-4 mb-6">
+      <div v-for="(environment, key) in environments" class="w-full md:w-1/2 xl:w-1/3 px-4 mb-6" :key="key">
         <div class="border border-grey rounded overflow-hidden">
            <div class="bg-white">
             <div class="border-t-4 border-teal p-4">
@@ -16,6 +16,7 @@
               <div class="text-grey-darker">{{ environment.label }}</div>
             </div>
           </div>
+
           <div>
             <button type="button" class="flex items-center justify-between text-white pl-1 bg-blue-light hover:bg-blue-dark focus:bg-blue-dark focus:outline-none focus:underline text-sm w-full text-left">
               <svg class="h-6 w-5 text-white mr-2 fill-current" role="presentation"><use xlink:href="icons.symbol.svg#objects__grip-handle"></use></svg>
