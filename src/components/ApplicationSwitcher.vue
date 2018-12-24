33998cf45fc35dd592a555d941fc630547d64ae3
Oliver Davies <oliver@oliverdavies.uk>
2018-12-24 12:44:34 +0000

    Fix error on switch

--- src/components/ApplicationSwitcher.vue
+++ src/components/ApplicationSwitcher.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div class="bg-grey-lightest rounded overflow-hidden">
-      <button type="button" class="p-2 focus:outline-none" :class="[mode == 'grid' ? 'bg-blue-light text-white' : 'text-grey-darkest']" @click="changeMode('grid')">
-        <svg class="h-5 w-5 fill-current"role="presentation"><use xlink:href="/img/icons.symbol.svg#actions__grid"></use></svg>
+      <button type="button" class="p-2 focus:outline-none" :class="[mode == 'grid' ? 'bg-blue-light text-white' : 'text-grey-darkest']" @click="$emit('display-changed', 'grid')">
+        <svg class="h-5 w-5 fill-current" role="presentation"><use xlink:href="/img/icons.symbol.svg#actions__grid"></use></svg>
         <span class="hidden">Grid</span>
       </button>
 
-      <button type="button" class="p-2 focus:outline-none" :class="[mode == 'list' ? 'bg-blue-light text-white' : 'text-grey-darkest']" @click="changeMode('list')">
+      <button type="button" class="p-2 focus:outline-none" :class="[mode == 'list' ? 'bg-blue-light text-white' : 'text-grey-darkest']" @click="$emit('display-changed', 'list')">
         <svg class="h-5 w-5 fill-current" role="presentation"><use xlink:href="/img/icons.symbol.svg#actions__list"></use></svg>
         <span class="hidden">List</span>
       </button>
@@ -19,18 +19,5 @@ export default {
   props: {
     mode: String,
   },
-
-  // data() {
-  //   return {
-  //     mode: 'grid'
-  //   }
-  // },
-
-  methods: {
-    changeMode(mode) {
-      this.mode = mode
-      this.$emit('display-changed', mode)
-    }
-  }
 }
 </script>
