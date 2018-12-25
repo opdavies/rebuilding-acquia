fb739b336a82ba468151cd0612d26663898fdccd
Oliver Davies <oliver@oliverdavies.uk>
2018-12-25 23:19:55 +0000

    Add title block component

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ Vue.config.productionTip = false
 import HeaderButtons from '@/components/HeaderButtons'
 import Sidebar from '@/components/Sidebar'
 import Navbar from '@/components/Navbar'
+import TitleBlock from '@/components/TitleBlock'
 
 Vue.component('header-buttons', HeaderButtons);
 Vue.component('navbar', Navbar);
 Vue.component('sidebar', Sidebar);
+Vue.component('title-block', TitleBlock);
 
 new Vue({
   render: h => h(App),
