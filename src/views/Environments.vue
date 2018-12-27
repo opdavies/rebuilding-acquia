2364f9dbc149d59d73be5589171775a5d92e1ad3
Oliver Davies <oliver@oliverdavies.uk>
2018-12-27 09:48:43 +0000

    Revert "Rename component"
    This reverts commit d20e2890f547c01e65a2f55870b579c41a915de0.

--- src/views/Environments.vue
+++ src/views/Environments.vue
@@ -75,7 +75,7 @@
 
 <script>
 import ApplicationTags from '@/components/Application/ApplicationTags'
-import EnvironmentCard from '@/components/Environment/EnvironmentCard'
+import EnvironmentCards from '@/components/Environment/EnvironmentCards'
 import QuickHelp from '@/components/Environment/QuickHelp'
 import TaskLog from '@/components/Environment/TaskLog/TaskLog'
 import ToggleHelp from '@/components/Environment/ToggleHelp'
@@ -84,7 +84,7 @@ import data from '@/data.json'
 export default {
   components: {
     ApplicationTags,
-    EnvironmentCard,
+    EnvironmentCards,
     QuickHelp,
     TaskLog,
     ToggleHelp,
