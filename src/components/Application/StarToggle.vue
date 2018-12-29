174988a7e35ab31eebf028da783c4ff005852679
Oliver Davies <oliver@oliverdavies.uk>
2018-12-29 16:59:34 +0000

    Move star

--- src/components/Application/StarToggle.vue
+++ src/components/Application/StarToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <button type="button" @click="starred = !starred" class="focus:outline-none" :class="[starred ? 'text-orange hover:text-orange-light focus:text-orange-light' : 'text-grey focus:text-orange hover:text-orange']">
     <span class="visuallyhidden">{{ !starred ? 'Star' : 'Unstar' }} {{ application.name }}</span>
-    <svg class="h-6 w-6 fill-current" role="presentation"><use :xlink:href="`/img/icons.symbol.svg#state__${starred ? 'starred' : 'unstarred'}`"></use></svg>
+    <svg class="h-6 w-6 fill-current -mr-1" role="presentation"><use :xlink:href="`/img/icons.symbol.svg#state__${starred ? 'starred' : 'unstarred'}`"></use></svg>
   </button>
 </template>
 
