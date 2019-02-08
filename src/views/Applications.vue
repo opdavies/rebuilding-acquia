a334e3a25f91d8ae789b4e6986e9e79704ba3325
Oliver Davies <oliver@oliverdavies.uk>
2019-02-08 14:32:10 +0000

    Update sidebar top margins

--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -17,7 +17,7 @@
     </div>
 
     <div class="mt-48">
-      <div class="-mt-10 md:-mt-4 flex flex-row-reverse h-full">
+      <div class="-mt-10 md:-mt-3 flex flex-row-reverse h-full">
         <div class="flex-1 p-4 lg:p-12 ml-16 lg:ml-56 overflow-x-hidden">
           <div class="mb-6">
             <div class="lg:flex lg:items-baseline mb-12">
--- src/views/Environment.vue
+++ src/views/Environment.vue
@@ -38,7 +38,7 @@
     </div>
 
     <div class="mt-48">
-      <div class="-mt-4 md:-mt-2 flex flex-row-reverse h-full">
+      <div class="-mt-3 md:-mt-1 flex flex-row-reverse h-full">
         <div class="flex-1 p-4 lg:p-12 ml-16 lg:ml-56 overflow-x-hidden">
           <div>
             <div>
