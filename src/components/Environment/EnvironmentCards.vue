23f620d026891ffeaf375af03c918fedda2a1ccd
Oliver Davies <oliver@oliverdavies.uk>
2019-03-06 18:04:04 +0000

    Adjust colours and spacing

--- src/components/Environment/EnvironmentCards.vue
+++ src/components/Environment/EnvironmentCards.vue
@@ -5,13 +5,13 @@
         <div class="border border-grey rounded overflow-hidden">
            <div class="bg-white">
             <div class="border-t-4 border-teal p-3">
-              <router-link :to="{ name: 'environment', params: { environmentName: key, id: id }}" class="flex items-baseline no-underline hover:underline focus:underline text-grey-darkest hover:text-blue-dark focus:text-blue-dark focus:outline-none">
-                <h2 class="mb-1">{{ environment.name }}</h2>
+              <router-link :to="{ name: 'environment', params: { environmentName: key, id: id }}" class="flex items-baseline no-underline hover:underline focus:underline text-grey-darker hover:text-blue-dark focus:text-blue-dark focus:outline-none mb-1">
+                <h2>{{ environment.name }}</h2>
                 <svg class="w-4 h-4 fill-current text-inherit ml-2" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" fill-rule="evenodd"/></svg>
               </router-link>
 
-              <div class="text-grey-darker mb-2">
-                <a href="#0" class="no-underline hover:underline focus:underline text-grey-darkest hover:text-blue-dark focus:text-blue-dark focus:outline-none">
+              <div class="mb-2">
+                <a href="#0" class="no-underline hover:underline focus:underline text-grey-darker hover:text-blue-dark focus:text-blue-dark focus:outline-none">
                   {{ environment.url }}
                 </a>
               </div>
