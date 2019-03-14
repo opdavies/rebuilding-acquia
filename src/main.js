f4fdb615aa0cafa7cfcd42fc6d67b43d14974aef
Oliver Davies <oliver@oliverdavies.uk>
2019-03-14 09:15:58 +0000

    Group and order imports

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import App from './App.vue'
+import HeaderButtons from '@/components/HeaderButtons'
+import Navbar from '@/components/Navbar'
 import routes from './routes'
+import Sidebar from '@/components/Sidebar'
+import TitleBlock from '@/components/TitleBlock'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
-
-import HeaderButtons from '@/components/HeaderButtons'
-import Sidebar from '@/components/Sidebar'
-import Navbar from '@/components/Navbar'
-import TitleBlock from '@/components/TitleBlock'
 
 Vue.component('header-buttons', HeaderButtons);
 Vue.component('navbar', Navbar);
