46a1271ea050496a2ea0f214981a906358bb65f4
Oliver Davies <oliver@oliverdavies.uk>
2019-03-15 14:06:41 +0000

    Fix text colour

--- src/components/Environment/ToggleHelp.vue
+++ src/components/Environment/ToggleHelp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hidden lg:block">
-    <button type="button" class="text-blue text-sm font-hairline flex items-center hover:underline focus:underline focus:outline-none" @click="$emit('toggle')">
+    <button type="button" class="text-blue-3 text-sm font-hairline flex items-end hover:underline focus:underline focus:outline-none" @click="$emit('toggle')">
       <svg class="h-5 w-5 mr-1 text-blue-3 fill-current" role="presentation"><use xlink:href="/img/icons.symbol.svg#actions__info"></use></svg>
       {{ hidden ? 'Show quick help' : 'Hide quick help' }}
     </button>
