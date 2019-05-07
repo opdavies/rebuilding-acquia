61461fd0b8ecd8b9f020960049797b0f1fc302d9
Oliver Davies <oliver@oliverdavies.uk>
2019-05-07 17:43:57 +0100

    Move default classes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app" class="flex flex-col flex-1">
-    <router-view class="flex flex-col flex-1"></router-view>
+  <div id="app" class="min-h-screen antialiased bg-gray-200 font-sans flex flex-col flex-1">
+    <div class="flex flex-col flex-1">
+      <router-view class="flex flex-col flex-1"></router-view>
+    </div>
   </div>
 </template>
 
